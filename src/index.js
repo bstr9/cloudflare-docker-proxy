a701d8a95783f23a20b20ec3053284a0456fce4b
bstr9 <82707826+bstr9@users.noreply.github.com>
2024-09-03 19:06:43 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "cloudflare-docker-proxy.baojiwei.workers.dev": "https://registry-cdn.uii-ai.com"
+  "cloudflare-docker-proxy.baojiwei.workers.dev": "https://106.15.194.45"
 };
 
 function routeByHosts(host) {
